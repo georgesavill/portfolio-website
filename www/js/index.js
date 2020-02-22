d3b2012cfa4de739c17df96b9cb3c6b9867043fe
georgesavill <gpsavill+github@gmail.com>
2020-02-22 17:25:47 +0000

    Change imageSlideshow to use a single div
    To prevent the problematic use of position:absolute, imageSlideshow()
    has been refactored to cycle through background images on a single div.

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,20 @@
-imageSlideshow(0, "./img/grain-app-", "a", "si", 4000);
-function imageSlideshow(i, imgUrl, imgClass, containerClass, delay) {
-    var imgs = document.getElementsByClassName(containerClass);
-    var imgCount = imgs.length;
-    for (var j = 0; j < imgCount; j++) {
-        imgs[j].style.opacity = "0";
-    }
-    if (i < (imgCount - 1)) { imgs[i+1].style.backgroundImage = 'url("' + imgUrl + (i + 1) + '.png")'; }
+imageSlideshow(
+    0,
+    [
+        "./img/grain-app-0.png",
+        "./img/grain-app-1.png",
+        "./img/grain-app-2.png",
+        "./img/grain-app-3.png",
+        "./img/grain-app-4.png",
+        "./img/grain-app-5.png"
+    ],
+    "grain-app-slideshow",
+    5000
+);
+
+function imageSlideshow(i,imgUrls,containerClass,delay){
+    document.getElementsByClassName(containerClass)[0].style.backgroundImage = 'url("' + imgUrls[i] + '")';
     i++;
-    if (i > imgCount) { i = 1; }
-    imgs[i-1].style.opacity = "1";
-    setTimeout(imageSlideshow, delay, i, imgUrl, imgClass, containerClass, delay);
-};
+    if (i > (imgUrls.length - 1)){i = 0;}
+    setTimeout(imageSlideshow,delay,i,imgUrls,containerClass,delay);
+}
