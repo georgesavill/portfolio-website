da4f57f3f5764929345885ff1b084dbe0fb703eb
georgesavill <gpsavill+github@gmail.com>
2020-02-22 17:34:36 +0000

    Add lightness to imageSlideshow()

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,7 @@
-imageSlideshow(
-    0,
-    [
-        "./img/grain-app-0.png",
-        "./img/grain-app-1.png",
-        "./img/grain-app-2.png",
-        "./img/grain-app-3.png",
-        "./img/grain-app-4.png",
-        "./img/grain-app-5.png"
-    ],
-    "grain-app-slideshow",
-    5000
-);
-
-function imageSlideshow(i,imgUrls,containerClass,delay){
-    document.getElementsByClassName(containerClass)[0].style.backgroundImage = 'url("' + imgUrls[i] + '")';
+imageSlideshow(0,"./img/grain-app-",6,"grain-app-slideshow",5000);
+function imageSlideshow(i,imgUrl,imgCount,containerClass,delay){
+    document.getElementsByClassName(containerClass)[0].style.backgroundImage = 'url("' + imgUrl + i + '.png")';
     i++;
-    if (i > (imgUrls.length - 1)){i = 0;}
-    setTimeout(imageSlideshow,delay,i,imgUrls,containerClass,delay);
+    if (i > (imgCount - 1)){i = 0;}
+    setTimeout(imageSlideshow,delay,i,imgUrl,imgCount,containerClass,delay);
 }
