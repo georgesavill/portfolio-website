3336ca80ad3e032c7c7c7cb6fbb20741b9d498d9
georgesavill <gpsavill+github@gmail.com>
2020-02-24 16:18:44 +0000

    Add initial content for Access database section

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 imageSlideshow(0,"./img/grain-app-",6,"grain-app-slideshow",5000);
+imageSlideshow(0,"./img/db-",4,"db-slideshow",5000);
 function imageSlideshow(i,imgUrl,imgCount,containerClass,delay){
     document.getElementsByClassName(containerClass)[0].style.backgroundImage = 'url("' + imgUrl + i + '.png")';
     i++;
